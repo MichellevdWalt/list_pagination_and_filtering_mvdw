75e9e1731c750360c466e4d6f7e772d5c607e1b8
Michelle <62473111+MichellevdWalt@users.noreply.github.com>
2020-04-04 13:28:38 +0200

    updated code

--- list_pagination_and_filtering_mvdw/js/script.js
+++ list_pagination_and_filtering_mvdw/js/script.js
@@ -103,24 +103,112 @@ function clearText() {
     
 }
 
+let results;
+
+
+
+function searchPagination(){
+     const paginationLinkList = document.querySelectorAll('.pagination li a');
+     const paginationLi = document.querySelector('.pagination li');
+    for(var i = 0; i<paginationLinkList.length; i += 1) {
+        paginationLinkList[i].style.display = "none";}
+     let resultsLength = results.length;
+        for (var j = 0; j< Math.ceil(resultsLength/10); j +=1){
+          let newLink = document.createElement("a");
+            newLink.textContent = `${j + 1}`;
+            newLink.type = "button";
+            newLink.setAttribute('href', '#');
+            newLink.id = `${j+1}`
+            newLink.classList.add("searchButton");
+            paginationLi.appendChild(newLink);
+            
+     }
+function showPageSearch() {
+    let searchPagButton = document.querySelectorAll('.searchButton');
+    for (var j = 0; j< searchPagButton.length; j+=1) {
+        searchPagButton[j].addEventListener ("click", (e) =>{
+        for (var i = 0; i < results.length; i +=1){
+            if (i < parseInt(e.target.textContent) *10 && i >= (parseInt(e.target.textContent)*10) - 10 ) {
+            results[i].parentNode.parentNode.style.display = ""; } else {
+            results[i].parentNode.parentNode.style.display = "none";}
+        }
+        }                             
+        )
+    }
+   searchPagButton[0].click();
+}
+
+showPageSearch();
+}
+        
 /**
 *Function to hide or display list elements according to input value after clicking the submit button.
 *for loop functions to loop through the entire array in list to find student names that includes the search input value. Uses @param {number} - counter which reflects the length of the array as calculated in the assignPage function.
 */
-function searchBar(){
+function searchButtonClick(){
      let searchInput = document.querySelector('.search-input');
      let searchButton = document.querySelector('.search-button');
      let studentName = document.querySelectorAll("h3");
+     let displayNoResults = document.createElement("p");
+     let header = document.querySelector(".page-header h2");
+     let paginationButtons = document.querySelectorAll('.pagination li a');
+     header.appendChild(displayNoResults);
+     displayNoResults.classList.add("paragraph");
+     displayNoResults.classList.add("no-results");
         searchButton.addEventListener("click", (e)=>{
+        results = "";
         let input = searchInput.value;
         for (var i = 0; i<counter; i +=1){
             if(studentName[i].textContent.includes(input)){
-                list[i].style.display = "";} else{
-                    list[i].style.display = "none";}
+                list[i].style.display = ""
+                studentName[i].classList.add("results");
+            } else{
+                list[i].style.display = "none";
+                if (studentName[i].className.includes("results")){
+                    studentName[i].classList.remove("results");
+                }        
     }
-           clearText(); 
-        });
-   
+             
+    clearText(); 
         }
-searchBar();
+        results = document.querySelectorAll('.results');
+            
+        });
+    
+     searchButton.addEventListener("click", (f)=>{
+         if (results.length === 0){
+         displayNoResults.textContent = "\n\ Sorry, no results found";
+         for (var j = 0; j < paginationButtons.length; j+= 1) {
+         paginationButtons[j].style.display = "none";
+        }
+        
+        } else if (results.length > 0){
+        let displayNoResults = document.querySelector(".no-results");
+        displayNoResults.textContent = " ";
+        searchPagination();}
+        });
+}
+    
+searchButtonClick();
+
+
+
+
+/**
+*Function to hide or display list elements according to input value after pressing enter whilst typing in the input field.
+*if statement functions to activate a click on searchButton and thus execute eventlistener from searchButtonClick() if enter(keycode 13) is hit. 
+*/
+function textInputEnter() {
+    let searchInput = document.querySelector('.search-input');
+    let searchButton = document.querySelector('.search-button');
+     searchInput.addEventListener("keyup", (e) => {
+    if (event.keyCode === 13) {
+    event.preventDefault();
+    searchButton.click();
+  }
+});
+}
+
+textInputEnter();
+    
 
